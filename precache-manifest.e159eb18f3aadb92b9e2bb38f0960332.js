07ae41698caa79a55aa5e3b90a7153e9d886b446
Begum V <115528938+Beg92@users.noreply.github.com>
2023-04-20 23:58:14 -0700

    Updates

---
+++ precache-manifest.e159eb18f3aadb92b9e2bb38f0960332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74649eeaf7c93c85bf83",
+    "url": "/stateful-hotel-filters-app/static/css/main.9e83d1d8.chunk.css"
+  },
+  {
+    "revision": "74649eeaf7c93c85bf83",
+    "url": "/stateful-hotel-filters-app/static/js/main.7d98a1f9.chunk.js"
+  },
+  {
+    "revision": "3394e4cf2397fd15105d",
+    "url": "/stateful-hotel-filters-app/static/js/runtime~main.38611570.js"
+  },
+  {
+    "revision": "6978b117402f10549e57",
+    "url": "/stateful-hotel-filters-app/static/js/2.daa065df.chunk.js"
+  },
+  {
+    "revision": "d846100d3c787b92f6b2df8127343c19",
+    "url": "/stateful-hotel-filters-app/index.html"
+  }
+];
